6f3a746f3d77116af463f38bc7bb045ace59ac25
imagine10255 <imagine10255@gmail.com>
2023-10-11 12:51:14 +0800

    upgrade 4.0.18

--- src/components/MyCarousel.tsx
+++ src/components/MyCarousel.tsx
@@ -1,26 +1,8 @@
-'use client'
-
 import BearCarousel, {BearSlideCard, IBearSlideItemData, BearSlideImage} from 'bear-react-carousel';
 import "bear-react-carousel/dist/index.css";
 import {useCallback} from 'react';
 
 const MyCarousel = () => {
-    const images = [
-        { id: 1, imageUrl: "/sample/01.webp" },
-        { id: 2, imageUrl: "/sample/02.webp" },
-        { id: 3, imageUrl: "/sample/03.webp" },
-        { id: 4, imageUrl: "/sample/04.webp" },
-        { id: 5, imageUrl: "/sample/05.webp" },
-        { id: 6, imageUrl: "/sample/06.webp" }
-    ];
-
-    const bearSlideItemData: IBearSlideItemData[] = images.map((row) => {
-        return {
-            key: row.id,
-            children: <BearSlideCard bgSize="100%" bgUrl={row.imageUrl} />
-        };
-    });
-
 
     const slideData = useCallback(() => {
         const dataList = [
